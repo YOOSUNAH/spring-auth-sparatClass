78d7fa85c2d057a0bc7519101a3f9f2a9e6efc68
YOOSUNAH <tjsdkyd4027@gmail.com>
2024-01-26 16:18:52 +0900

    로그인 구현 : JWT

--- src/main/java/com/sparta/springauth/controller/UserController.java
+++ src/main/java/com/sparta/springauth/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.sparta.springauth.controller;
 
+import com.sparta.springauth.dto.LoginRequestDto;
 import com.sparta.springauth.dto.SignupRequestDto;
 import com.sparta.springauth.service.UserService;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,8 +28,20 @@ public class UserController {
     }
 
     @PostMapping("/user/signup")
-    public String signup(SignupRequestDto signupRequestDtp){
+    public String signup(SignupRequestDto signupRequestDtp) {
         userService.signup(signupRequestDtp);
         return "redirect:/api/user/login-page"; // 다시 로그인하라는 의미에서 로그인 페이지로 전환
+    }
+
+    @PostMapping("/user/login")
+    public String login(LoginRequestDto loginRequestDto, HttpServletResponse res) {
+        try {
+            userService.login(loginRequestDto, res);
+        } catch (Exception e) {
+            return "redirect:/api/user/login-page?error";
+//            e.getMessage();
+        }
+
+        return "redirect:/"; // 다시 메인으로
     }
 }
--- src/main/java/com/sparta/springauth/service/UserService.java
+++ src/main/java/com/sparta/springauth/service/UserService.java
@@ -1,24 +1,26 @@
 package com.sparta.springauth.service;
 
+import com.sparta.springauth.dto.LoginRequestDto;
 import com.sparta.springauth.dto.SignupRequestDto;
 import com.sparta.springauth.entity.User;
 import com.sparta.springauth.entity.UserRoleEnum;
+import com.sparta.springauth.jwt.JwtUtil;
 import com.sparta.springauth.repository.UserRepository;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
 
-    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
-        this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
-    }
 
     // ADMIN_TOKEN
     private final String ADMIN_TOKEN = "AAABnvxRVklrnYxKZ0aHgTBcXukeZygoC";
@@ -59,5 +61,25 @@ public class UserService {
         // 사용자 등록
         User user = new User(username, password, email, role); // 여기 들어오는 role은 바로 위에서 ADMIN 권한 enum 값이 들어가있는 role이다.
         userRepository.save(user);  // 저장 됨.
+    }
+
+    public void login(LoginRequestDto requestDto, HttpServletResponse res) {
+        String username = requestDto.getUsername();
+        String password = requestDto.getPassword();
+
+        // 사용자 확인
+        User user =  userRepository.findByUsername(username).orElseThrow(
+        () -> new IllegalArgumentException("등록된 사용자가 없습니다.")
+        );
+
+        // 비밀번호 확인
+        if(!passwordEncoder.matches(password, user.getPassword())){  // mathces 기능 (평문, 암호화된 데이터-> entity객체로 가져오기)
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        }
+
+        // JWT 생성 및 쿠키에 저장 후 response 객체에 추가 (addJwtToCookie 통해 response객체에 추가됨)
+        String token = jwtUtil.createToken(user.getUsername(), user.getRole());
+        jwtUtil.addJwtToCookie(token, res);
+
     }
 }
